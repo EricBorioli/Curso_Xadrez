d07e0cce9a84dfed45a17f4f4e3861aa0e0dc682
Eric Borioli <eborioli@gmail.com>
2020-09-14 11:02:39 -0300

    Criado a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,7 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ola mundo");
+            Posicao p;
+                p = new Posicao(3, 4);
+            Console.WriteLine(p);
             Console.ReadKey();
         }
     }
